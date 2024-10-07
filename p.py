20e9d0200a309e8e058f2e8011bdb0bc0ce2f34f
Jaanane Catherine Mahadevan <113246343+jaycee7903@users.noreply.github.com>
2024-10-07 21:14:29 +1100

    Add files via upload

---
+++ p.py
@@ -0,0 +1,20 @@
+
+def calculate_perimeter_of_rectangle(width, length):
+    p = (width + length) * 2
+    return p
+
+width = input("Enter the width of the rectangle: ")
+length = input("Enter the lenght of the rectangle: ")
+if(not (width.isnumeric()) and not(length.isnumeric())):
+    print("Width and length have to be number. Program stopped.")
+else:
+    #convert width and length to integer
+    width = int(width)
+    length = int(length)
+
+    #check if width or length is 0
+    if(width == 0 or length == 0):
+        print("Width and length cannot be 0. Program stopped")
+    else:
+        p = calculate_perimeter_of_rectangle(width, length)
+        print("Perimeter of the rectangle is " + str(p))
